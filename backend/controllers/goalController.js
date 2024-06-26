c806e04570e3a98d97e6e03daabfa064fae17bb5
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-06-26 10:38:56 +0000

    frontend ui

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
-// @desk Get GOals 
+const User = require('../models/userModel')
+// @desk Get GOals  
 // @route GET /api/goals
 // @access Private 
 
 const getGoals = asyncHandler(async (req,res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals) 
 })
 // @desk Set GOals 
@@ -21,6 +22,7 @@ const setGoal = asyncHandler(async (req,res) => {
 
     const goal = await Goal.create ({
         text: req.body.text,
+        user: req.user.id ,
     })
     res.status(200).json(goal)
 })
@@ -34,6 +36,21 @@ const updateGoal = asyncHandler(async (req,res) => {
     if(!goal) {
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user =  await User.findById(req.user.id)
+    
+    // check for user 
+
+    if(!user){
+        res.status(401)
+        throw new Error('user not found')
+    }
+     
+    // making sure the logged in user matched the goal user
+    if(goal.user.toString() !== user.id){
+       res.status(401)
+       throw new Error('User not Authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
@@ -51,6 +68,21 @@ const deleteGoal = asyncHandler (async (req,res) => {
     if(!goal) {
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user =  await User.findById(req.user.id)
+    
+    // check for user 
+
+    if(!user){
+        res.status(401)
+        throw new Error('user not found')
+    }
+     
+    // making sure the logged in user matched the goal user
+    if(goal.user.toString() !== user.id){
+       res.status(401)
+       throw new Error('User not Authorized')
     }
     
     res.status(200).json({ id: req.params.id })
---
+++ backend/controllers/userController.js
@@ -0,0 +1,124 @@
+const jwt  = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+
+// @desk Register New user
+// @route Post /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    //check if the user exists
+
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already Exist') 
+    }
+
+    //Hash password
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user  
+
+    const user = await User.create ({
+        name,
+        email,
+        password: hashedPassword  //set the password to the hashed password when creating a user
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user.id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('invalid data')
+    }
+})
+
+
+// @desk Authenticate a user
+// @route Post /api/users/login
+// @access Public
+
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('invalid credential')
+    }
+})
+
+   
+// @desk Get User data
+// @route Get /api/users/me
+// @access Private
+
+const getMe = asyncHandler(async (req, res) => {
+    const {email, _id, name} = await User.findById(req.user.id)
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
+}) 
+
+// generate JWT 
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
+
+
+
+/// i want to delete the first user it worked hurrayyy Alhamdulillah 
+
+const deleteUser = asyncHandler (async (req,res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if(!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+    
+    res.status(200).json({ id: req.params.id,
+        name: user.name
+     })
+})
+   
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+    deleteUser,
+}
